f5afd5842ea02c020bd20e31c6728e0c2940d7a5
A <a@a-System-Product-Name.(none)>
2012-11-01 18:19:47 +0400

    fixes

--- db/migrate/20121012173019_remove_integer_from_pictures.rb
+++
@@ -1,8 +0,0 @@
-class RemoveIntegerFromPictures < ActiveRecord::Migration
-  def up
-  	remove_column :pictures, :integer
-  end
-
-  def down
-  end
-end
--- db/migrate/20121012173334_remove_sdfds_from_pictures.rb
+++
@@ -1,9 +0,0 @@
-class RemoveSdfdsFromPictures < ActiveRecord::Migration
-  def up
-    remove_column :pictures, :integer
-  end
-
-  def down
-    add_column :pictures, :integer, :string
-  end
-end
