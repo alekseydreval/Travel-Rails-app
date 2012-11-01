fc2daa9db5c71a399621dd3d996969b675515f1e
A <a@a-System-Product-Name.(none)>
2012-11-01 19:15:11 +0400

    fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < ActionController::Base
   # private
   
   def current_user
-    session[:user_id] = nil
   	@current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end
   
