1c058ab9cf1cd8141f0665d0a1b5ee724c6c899d
A <a@a-System-Product-Name.(none)>
2012-11-01 19:11:26 +0400

    fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   
   def current_user
     session[:user_id] = nil
-  	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+  	@current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end
   
   def redirect_to_login
