90315ec9bbcc03a4d9ecc4c90bd4a7c7699c0a47
dozenthoughts <aldesk8@yandex.ru>
2012-11-02 18:06:35 +0400

    fixes

--- config/application.rb
+++ config/application.rb
@@ -7,10 +7,13 @@ if defined?(Bundler)
   Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
+
 end
 
 module Travel
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
