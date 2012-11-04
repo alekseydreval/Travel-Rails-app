67bb5ffcb354615f7900bf092f13bad10f89cb8c
dozenthoughts <aldesk8@yandex.ru>
2012-11-04 23:07:51 +0400

    fixes

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ end
 module Travel
   class Application < Rails::Application
     config.assets.initialize_on_precompile = false
+    config.assets.precompile += [/^[a-zA-Z]*\..*/]
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
